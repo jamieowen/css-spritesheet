2892f1ccd1f36aeaf1387ded915ff5aabeceac2f
jamieowen <hello@jamieowen.com>
2016-04-17 22:25:43 +0100

    wip..

--- SpriteSheet.js
+++ SpriteSheet.js
@@ -1,8 +1,10 @@
 
 var jss = require( 'jss' );
 
+console.log( jss );
 var SpriteSheet = function(){
 
+	this.style = null;
 
 };
 
@@ -17,17 +19,53 @@ SpriteSheet.prototype = {
 
 		if( json.frames ){
 
-			var style = {};
+			var styles = {};
+			var frame;
+
+			var className;
+			var image = 'example/sprites/' + json.meta.image;
+			var background = 'url(' + image + ');';
+
+			var scale = 0.5;
+			var sW = json.meta.size.w * scale;
+			var sH = json.meta.size.h * scale;
 
 			for( var key in json.frames ){
 
-				style[ key ] = {
+				frame = json.frames[key].frame;
 
-				}
+				console.log( "FRAMES: ", frame );
+
+				className = 'sprite-' + key.replace( '.', '' );
+
+				styles[ className ] = {
+
+					background: background,
+					'background-position': -( frame.x * scale ) + 'px ' + -( frame.y * scale ) + 'px',
+					'background-size': sW + 'px ' + sH + 'px',
+					width: ( frame.w * scale ) + 'px',
+					height: ( frame.h * scale ) + 'px',
+					//width: '100px',
+					//height: '100px',
+					display: 'inline-block'
+					//position:'absolute',
+					//left: '0px',
+					//top: '0px',
+					//'background-color': '#fff'
+
+				};
 
 			}
-		}
 
+			this.style = jss.createStyleSheet(styles);
+			this.style.attach();
+
+			console.log( json );
+			console.log( styles );
+			console.log( this.style );
+
+
+		}
 	}
 
 };
--- example/index.js
+++ example/index.js
@@ -4,6 +4,8 @@ var Loader = require( 'resource-loader' );
 
 window.onload = function(){
 
+	document.body.style.backgroundColor = '#000';
+
 	var loader = new Loader();
 	loader.add( 'example/sprites/spritesheet@2.json' );
 	loader.load( function( res ){
@@ -15,7 +17,25 @@ window.onload = function(){
 		var spritesheet = new SpriteSheet();
 		spritesheet.load( json );
 
+
+		var classes = Object.keys( spritesheet.style.classes );
+
+		classes.forEach( function( className ){
+
+			className = spritesheet.style.classes[ className ];
+			var ele = document.createElement( 'div' );
+			//ele.style.position = 'absolute';
+			//ele.style.display = 'block';
+			ele.classList.add( className );
+			document.body.appendChild( ele );
+
+		} );
+
+		console.log( classes );
+
 		console.log( json );
+
+		console.log( spritesheet );
 
 	})
 
